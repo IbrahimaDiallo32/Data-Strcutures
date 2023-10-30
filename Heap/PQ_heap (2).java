8f333aa242a1b021fda397359f40efca047c4e0d
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2023-10-30 16:31:18 -0400

    Add files via upload

---
+++ Heap/PQ_heap (2).java
@@ -0,0 +1,55 @@
+// Name: <Ibrahima Diallo>
+// Class: CS 3305/04
+// Term: Spring 2023
+// Instructor: Dr. Haddad
+// Assignment: 7
+// IDE Name: <IntelliJ>
+// Generic code for class PQ_heap for Assignment 7
+public class PQ_heap <E extends Comparable<E>>
+{
+    private Heap<E> heapObject = new Heap<>();
+    // Constructor method
+    PQ_heap() {
+
+    }
+
+    // Return true if priority queue is empty; otherwise return false
+    public boolean is_empty() {
+        return heapObject.getSize() == 0;
+    }
+
+    // Return true if priority queue is full; otherwise return false
+    public boolean is_full() {
+        return heapObject.getSize() >= heapObject.CAPACITY;
+    }
+
+    // Return (don't remove) the front element from the priority queue
+    // Precondition: priority queue is not empty.
+    public E front() {
+        if(heapObject.getSize() == 0){
+            System.out.println("Sorry, the Queue is empty.");
+        }
+        return heapObject.front();
+    }
+
+    // return number of elements in the queue
+    public int size() {
+        return heapObject.getSize();
+    }
+
+    // Remove the largest value from this priority queue and return it.
+    // Precondition: priority queue is not empty.
+    public E dequeue() {
+        return heapObject.remove();
+    }
+
+    // Inserts the 'value' into the priority queue.
+    // Precondition: priority queue is not full
+    public void enqueue(E value) {
+        heapObject.add(value);
+    }
+    //this method will print the priority queue in order
+    public void printQueue(){
+        heapObject.printQueue();
+    }
+}
